6a236e4949a34b602dea9888e5ba551d1f138d54
Kai Kok, Chew <kaikok@gmail.com>
2023-11-01 13:12:50 +0800

    Added the Daily_config object.

---
+++ daily_config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+
+class Daily_config:
+    def __init__(self):
+        load_dotenv()
+        self.actions_file = os.environ["ACTIONS_FILE"]
+        self.cards_file = os.environ["CARDS_FILE"]
+        self.api_key = os.environ["API_KEY"]
+        self.token = os.environ["TOKEN"]
+        self.board_name = os.environ["BOARD_NAME"]
+        self.done_list_name = os.environ["DONE_LIST_NAME"]
+        self.archival_board_name = os.environ["ARCHIVAL_BOARD_NAME"]
