5a40adcbeb5e7531141bc01c573d8546dfeda759
Kai Kok, Chew <kaikok@gmail.com>
2023-11-15 11:35:25 +0800

    Sketched out the high level logic of sync card task.

--- sync_cards.py
+++ sync_cards.py
@@ -1,2 +1,5 @@
-def perform_sync_cards(context, mocked_daily_config):
-  pass
+def perform_sync_cards(context, config):
+    context["card_sync_lookup"] = \
+        load_card_sync_lookup(config)
+    context["card_sync_lookup"] = find_sync_new_cards(context, config)
+    sync_all_cards(context, config)
