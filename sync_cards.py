339ffd71687fe8993fa7e65f1ca238fcb342bef2
Kai Kok, Chew <kaikok@gmail.com>
2023-11-15 11:43:42 +0800

    Added test stubs for the required methods to implement sync cards.

--- sync_cards.py
+++ sync_cards.py
@@ -1,5 +1,6 @@
 def perform_sync_cards(context, config):
-    context["card_sync_lookup"] = \
-        load_card_sync_lookup(config)
-    context["card_sync_lookup"] = find_sync_new_cards(context, config)
-    sync_all_cards(context, config)
+    # context["card_sync_lookup"] = \
+    #     load_card_sync_lookup(config)
+    # context["card_sync_lookup"] = find_sync_new_cards(context, config)
+    # sync_all_cards(context, config)
+    pass
--- tests/test_sync_cards.py
+++ tests/test_sync_cards.py
@@ -1,11 +1,17 @@
 from sync_cards import perform_sync_cards
 
 
-class Test_sync_cards:
-    def test_file_exist(self):
-        pass
-
-    def test_sync_cards(self, mocker):
+class Test_perform_sync_cards:
+    def test_method_exist(self, mocker):
         mocked_daily_config = mocker.Mock()
         context = {}
         perform_sync_cards(context, mocked_daily_config)
+
+class Test_load_card_sync_lookup:
+    pass
+
+class Test_find_sync_new_cards:
+    pass
+
+class Test_sync_all_cards:
+    pass
