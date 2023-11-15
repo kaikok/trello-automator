67aa4b08dadad68d89d44cb634ae7bec873bcb2b
Kai Kok, Chew <kaikok@gmail.com>
2023-11-15 11:18:20 +0800

    Renamed sync cards and added empty perform method for the task.

---
+++ sync_cards.py
@@ -0,0 +1,2 @@
+def perform_sync_cards(context, mocked_daily_config):
+  pass
--- tests/test_sync_cards.py
+++ tests/test_sync_cards.py
@@ -1,6 +1,11 @@
-import sync_card
+from sync_cards import perform_sync_cards
 
 
-class Test_sync_card:
+class Test_sync_cards:
     def test_file_exist(self):
         pass
+
+    def test_sync_cards(self, mocker):
+        mocked_daily_config = mocker.Mock()
+        context = {}
+        perform_sync_cards(context, mocked_daily_config)
